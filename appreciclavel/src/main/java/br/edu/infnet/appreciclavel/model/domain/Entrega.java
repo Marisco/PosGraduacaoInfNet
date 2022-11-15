d668e3b41e1a9916b6f41175f838b5d431d1293a
Christiano Silveira Marisco <cmarisco@mpes.mp.br>
2022-11-15 10:49:34 -0300

    feat:(FEATURE3) - Persistencia em banco - Banco de dados e a camada de repository 2

--- appreciclavel/src/main/java/br/edu/infnet/appreciclavel/model/domain/Entrega.java
+++ appreciclavel/src/main/java/br/edu/infnet/appreciclavel/model/domain/Entrega.java
@@ -3,13 +3,26 @@ package br.edu.infnet.appreciclavel.model.domain;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "taluminio")
 public class Entrega {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String descricao;
 	private LocalDateTime data;
 	private boolean web;
+	@Transient
 	private Reciclador reciclador;
+	@Transient
 	private List<Reciclavel> reciclaveis;
 
 	public Entrega() {
--- appreciclavel/src/main/java/br/edu/infnet/appreciclavel/model/service/EntregaService.java
+++ appreciclavel/src/main/java/br/edu/infnet/appreciclavel/model/service/EntregaService.java
@@ -1,37 +1,30 @@
 package br.edu.infnet.appreciclavel.model.service;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.edu.infnet.appreciclavel.model.domain.Entrega;
-import br.edu.infnet.appreciclavel.model.domain.Reciclavel;
+import br.edu.infnet.appreciclavel.model.repository.EntregaRepository;
 
 @Service
 public class EntregaService {
 	
-	private static Map<Integer, Entrega> mapa = new HashMap<Integer, Entrega>();
-	private static Integer id = 1;
-	private static Integer idReciclavel = 1;
-
+	@Autowired
+	private  EntregaRepository entregaRepository;	
+	
 	public void incluir(Entrega entrega) {
 
-		entrega.setId(id++);
-		List<Reciclavel> reciclaveis = entrega.getReciclaveis();
-		reciclaveis.forEach(r -> r.setId(idReciclavel++));
-
-		mapa.put(entrega.getId(), entrega);
+		entregaRepository.save(entrega);	
 	}
 
 	public Collection<Entrega> obterLista() {
-		return mapa.values();
+		return (Collection<Entrega>) entregaRepository.findAll();
 	}
 
 	public void excluir(Integer id) {
-		mapa.remove(id);
+		entregaRepository.deleteById(id);
 	}
 
 }
